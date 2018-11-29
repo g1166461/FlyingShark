0c66cf545a2a882bbc8e64bad626c644c0092f5d
charleneccc <g1165973@study.ouhk.edu.hk>
2018-11-30 00:40:08 +0800

    - add preference page(set difficulty and setting)

---
+++ app/src/main/java/comps413f/android/flyingandroid/FlyingAndroidPrefActivity.java
@@ -0,0 +1,28 @@
+package comps413f.android.flyingandroid;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+
+public class FlyingAndroidPrefActivity extends PreferenceActivity {
+
+    /** Preference activity for showing the preference screen */
+
+        /** Called when the activity is first created. */
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            getFragmentManager().beginTransaction()
+                    .replace(android.R.id.content, new EditorPreferenceFragment())
+                    .commit();
+        }
+
+        /** Subclass of PreferenceFragment to add preferences from resource. */
+        public static class EditorPreferenceFragment extends PreferenceFragment {
+            @Override
+            public void onCreate(final Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                addPreferencesFromResource(R.xml.game_prefs);
+            }
+        }
+    }
+
