75b15a2a2543a4377651be5c6c0619f0e90fb6b8
Lenovo-PC\lim.h.h <lim.h.hung@gmail.com>
2018-12-01 15:53:33 +0800

    Killer rotated

--- app/src/main/java/comps413f/android/flyingandroid/Killer.java
+++ app/src/main/java/comps413f/android/flyingandroid/Killer.java
@@ -1,13 +1,29 @@
 package comps413f.android.flyingandroid;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 
 public class Killer extends Sprite {
     private static final float INITIAL_DX = 3;  // Initial velocity in vertical direction
     private float dx;  // y velocity of the flying android object
+    private Bitmap  killerPicture;
+    private Drawable modifiedPicture;
+    private Bitmap killerRotatedPicture;
     /** Constructor. */
     public Killer(Context context) {
-        drawable = context.getResources().getDrawable(R.drawable.spaceship);
+        Matrix matrix = new Matrix();
+
+        matrix.postRotate(270);
+        killerPicture=BitmapFactory.decodeResource(context.getResources(), R.drawable.spaceship);
+        int scaledWidth = (int) (killerPicture.getWidth() * (FlyingAndroidView.arenaHeight / (float) killerPicture.getHeight()))/10;
+        killerPicture = Bitmap.createScaledBitmap(killerPicture, scaledWidth, FlyingAndroidView.arenaHeight/10, true);
+        killerRotatedPicture=Bitmap.createBitmap(killerPicture,0,0,scaledWidth,FlyingAndroidView.arenaHeight/10,matrix,true);
+        modifiedPicture=new BitmapDrawable(context.getResources(),killerRotatedPicture);
+        drawable = modifiedPicture;
         dx = Background.SpeedXMagnitude;
         setPosition(FlyingAndroidView.arenaHeight/2,(float)(Math.random()*2)*(FlyingAndroidView.arenaHeight/2));
     }
