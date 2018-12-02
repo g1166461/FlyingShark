5ccc22c799a8c2b2aa149238fcbfe66936d51f24
Lenovo-PC\lim.h.h <lim.h.hung@gmail.com>
2018-12-02 11:42:47 +0800

    del menu button menu

--- app/src/main/java/comps413f/android/flyingandroid/MainActivity.java
+++ app/src/main/java/comps413f/android/flyingandroid/MainActivity.java
@@ -30,35 +30,6 @@ public class MainActivity extends Activity {
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    /**
-     * Handles the option menu selection. This method is called when an options
-     * menu item is selected.
-     */
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-        case R.id.action_restart:
-            animationView.newGame(false);
-            break;
-
-            case R.id.about:
-                new AlertDialog.Builder(this)
-                        .setTitle(R.string.about_title)
-                        .setMessage(R.string.about_msg)
-                        .setNeutralButton(android.R.string.ok, null)
-                        .show();
-                break;
-
-        }
-        return false;
-    }
 
     /** Resumes the animation. This method is called when the activity is resumed. */
     @Override
